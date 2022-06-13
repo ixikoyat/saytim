7d9ebd4d533b84c03b40f91c2eb11c5ab0ea764e
Ilyosova Xikoyat <ixikoyat@gmail.com>
2022-06-13 16:58:20 +0500

    sdsv

---
+++ my.js
@@ -0,0 +1,15 @@
+// let yuzi = document.getElementById("koloni2");;
+// let a =document.getElementsByTagName("h1");
+// let b=document.getElementsById("koloni");
+// yuzi.innerHTML= "Qalesn";
+// let uni = document.querySelector(".rang1");
+// uni.classList.remove("rang1");
+// uni.classList.add("rang2")
+
+let app = document.querySelector(".Uzmovi");
+
+app.href="https://www.youtube.com/";
+app.width='200'
+console.log(app.href);
+
+
